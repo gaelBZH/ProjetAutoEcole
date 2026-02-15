1955fe550c42832b1344548314fa9eba5d6e6ba7
Ronan LE ROUZIC <ronan.lerouzic@outlook.fr>
2026-02-15 14:35:23 +0100

    Ajout de 2 autres fichiers cs pour que les autres dossiers que j'ai crée s'affiche

---
+++ ProjetAutoEcoleS4/ProjetAutoEcoleS4/Data/Database.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace ProjetAutoEcoleS4.Data
+{
+    public class Database
+    {
+        private static string connectionString =
+            "server=localhost;database=AutoEcole;uid=root;pwd=;";
+
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
